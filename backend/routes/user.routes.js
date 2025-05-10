935c1b0ae682ff6ce67fe77eb2ec4823b1efe726
VikashPal88 <vs700034@gmail.com>
2025-05-10 13:51:46 +0530

    first commit

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,32 @@
+import { Router } from "express";
+import * as userController from "../controllers/user.controller.js";
+import { body } from "express-validator";
+import { authUser } from "../middleware/auth.middleware.js";
+
+const router = Router();
+
+router.post(
+  "/register",
+  body("email").isEmail().withMessage("Email must be a valid email address"),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
+  userController.createUserController
+);
+
+router.post(
+  "/login",
+  body("email").isEmail().withMessage("Email must be a valid email address"),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
+  userController.loginController
+);
+
+router.get("/profile", authUser, userController.profileController);
+
+router.get("/logout", authUser, userController.logoutController);
+
+router.get("/all", authUser, userController.getAllUsersController);
+
+export default router;
